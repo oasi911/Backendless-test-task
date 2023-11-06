c2c67009112107f822aa91e932cea418cb3aa0b8
Anton <sokolanton.1998@gmail.com>
2023-11-06 16:22:48 +0200

    added Tabs component + main router logic

---
+++ src/components/Tabs.js
@@ -0,0 +1,39 @@
+import { React } from "react";
+import { useLoaderData, Link } from "react-router-dom";
+import css from "./Tabs.module.css";
+
+export function loader() {
+  return fetch("/tabs.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response wasn't ok");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(
+        "There was a problem with the fetch operation:",
+        error.message
+      );
+    });
+}
+
+const Tabs = () => {
+  const tabs = useLoaderData();
+
+  return (
+    <div className={css.container}>
+      <nav>
+        {tabs
+          .sort((a, b) => a.order - b.order)
+          .map((tab) => (
+            <Link className={css.button} key={tab.order} to={tab.id}>
+              {tab.title}
+            </Link>
+          ))}
+      </nav>
+    </div>
+  );
+};
+
+export default Tabs;
