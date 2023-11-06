ed2e72526237fb6293eb030ea8e86233cc42d3b2
Anton <sokolanton.1998@gmail.com>
2023-11-06 21:29:18 +0200

    Update gh-pages

---
+++ build/static/js/273.011486ea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbackendless_test_task=self.webpackChunkbackendless_test_task||[]).push([[273],{273:(s,e,l)=>{l.r(e),l.d(e,{default:()=>t});l(791);var n=l(184);const t=()=>(0,n.jsx)(n.Fragment,{children:(0,n.jsxs)("ul",{children:[(0,n.jsx)("li",{children:"Dummy"}),(0,n.jsx)("li",{children:"List"}),(0,n.jsx)("li",{children:"Content"}),(0,n.jsx)("li",{children:"Content"})]})})}}]);
+//# sourceMappingURL=273.011486ea.chunk.js.map
