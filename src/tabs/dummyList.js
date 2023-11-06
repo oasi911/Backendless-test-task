20cf3aae97584ac8b6390d2260b566dd73a0d16c
Anton <sokolanton.1998@gmail.com>
2023-11-06 17:00:23 +0200

    added tabs content

---
+++ src/tabs/dummyList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DummyList = () => (
+  <>
+    <ul>
+      <li>Dummy</li>
+      <li>List</li>
+      <li>Content</li>
+      <li>Content</li>
+    </ul>
+  </>
+);
+
+export default DummyList;
